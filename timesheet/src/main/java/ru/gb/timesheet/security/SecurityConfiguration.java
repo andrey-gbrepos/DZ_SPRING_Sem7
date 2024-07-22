966245142ac8e4fa930e161e97abf83fd1fe25d1
andrey-gbrepose <yandire@yandex.ru>
2024-07-22 08:26:06 +0300

    first commit

---
+++ timesheet/src/main/java/ru/gb/timesheet/security/SecurityConfiguration.java
@@ -0,0 +1,33 @@
+package ru.gb.timesheet.security;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        return http
+                .authorizeHttpRequests(requests -> requests
+                        .requestMatchers("/home/timesheets/**").hasAuthority("user")
+                        .requestMatchers("/home/projects/**").hasAuthority("admin")
+                        .requestMatchers("/projects/**", "/timesheets/**" ).hasAuthority("rest")
+                        .anyRequest().authenticated()
+                )
+                .formLogin(Customizer.withDefaults())
+                .build();
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+}
